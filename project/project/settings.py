60744231e8fac1a3bf6f0d129a74ddd49a6494a0
Hi-star <110157158+harshituprety@users.noreply.github.com>
2026-08-03 16:11:00 +0530

    Update settings.py
    Prevent exposure of Django SECRET_KEY in repository.
    Removed hardcoded SECRET_KEY usage and updated configuration to use environment variables.

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%(z21#dcon7x88&%+-=v))h-=vxpqt3rl#qjkxxn)346d=udlq'
+SECRET_KEY = 'your-super-secret-unshared-key-here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
